3801dfee869fc8f576d12c811d3976aad80353e6
tomy807 <tomy807@naver.com>
2023-04-13 14:00:15 +0900

    Fix Decompose

--- include/Decompose/Decompose.hpp
+++ include/Decompose/Decompose.hpp
@@ -12,11 +12,9 @@ public:
         assert(target.rows() == target.cols());
         const int n = target.rows();
 
-        // L과 U를 초기화한다.
         L = Matrix<T>(n, n);
         U = Matrix<T>(n, n);
 
-        // U의 첫 번째 행을 A의 첫 번째 행으로 설정한다.
         for (int j = 0; j < n; j++) {
             U(0, j) =target(0, j);
         }
@@ -25,14 +23,12 @@ public:
         }
         for (int i = 1; i < n; i++) {
                 for (int j = i; j < n; j++) {
-                    // U의 i번째 행 계산
                     T sum = 0;
                     for (int k = 0; k < i; k++) {
                         sum += L(i, k) * U(k, j);
                     }
                     U(i, j) = target(i, j) - sum;
 
-                    // L의 j번째 열 계산
                     if (j == i) {
                         L(i, j) = 1;
                     } else {
